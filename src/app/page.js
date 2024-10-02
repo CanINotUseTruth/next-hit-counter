863bab5658fc629f041e0f884e57543284dea06e
Truth <tobyrutherfordfb@gmail.com>
2024-10-02 22:25:29 +1000

    Complete

--- src/app/page.js
+++ src/app/page.js
@@ -1,33 +1,20 @@
 import React from 'react';
 
-import {
-  readFile,
-  writeFile,
-} from '../helpers/file-helpers';
+import HitCounter from '../components/HitCounter';
+import Censored from '../components/Censored/Censored';
 
 const DATABASE_PATH = '/src/database.json';
-
-/*
-`readFile` takes 1 argument:
-• the path to the file:
-
-readFile('/path/to/file');
-
-`writeFile` takes 2 arguments:
-• The path to the file
-• The new contents for the file
-
-writeFile(
-  '/path/to/file',
-  '{ "hello": "world" }'
-);
-*/
 
 function Home() {
   return (
     <main>
       <h1>Welcome!</h1>
-      <p>You are visitor number X.</p>
+      <p>
+        You are visitor number{' '}
+        <Censored>
+          <HitCounter />
+        </Censored>
+      </p>
     </main>
   );
 }
